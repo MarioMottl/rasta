6d99df0080bcbe25a55e057f052f4d7b007a588d
Mario Mottl <mariomottl17@gmail.com>
2024-01-15 01:06:50 +0100

    Initialize Rust project with Discord bot base
    A new Rust project for a Discord bot has been set up. The main module includes the bot's logic, such as handling events and providing commands for the bot. A ping command has been implemented for basic interactivity. The project's .gitignore file and .idea folder's contents have also been updated to fit the project's needs.

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod commands;
+
+use std::env;
+use serenity::all::{CreateInteractionResponse, CreateInteractionResponseMessage, GuildId, Interaction};
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+            else {
+                println!("Sent message");
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId::new(421740455839924226);
+
+        let commands = guild_id.set_commands(&ctx.http, vec![commands::ping::register()]).await.expect("Failed to register commands");
+
+        println!("I now have the following guild slash commands: {commands:#?}");
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            println!("Received command interaction: {command:#?}");
+
+            let content = match command.data.name.as_str() {
+                "ping" => Some(commands::ping::run(&command.data.options())),
+                _ => Some("not implemented :(".to_string()),
+            };
+
+            if let Some(content) = content {
+                let data = CreateInteractionResponseMessage::new().content(content);
+                let builder = CreateInteractionResponse::Message(data);
+                if let Err(why) = command.create_response(&ctx.http, builder).await {
+                    println!("Cannot respond to slash command: {why}");
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
